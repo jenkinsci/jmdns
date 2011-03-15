3c64581c5b72925afc1adc3377bbbb0da37e6145
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-03-14 23:57:01 -0700

    fixed a test failure

--- src/test/java/javax/jmdns/test/DNSStatefulObjectTest.java
+++ src/test/java/javax/jmdns/test/DNSStatefulObjectTest.java
@@ -60,7 +60,7 @@ public class DNSStatefulObjectTest {
 
     @Test
     public void testWaitAndSignal() throws InterruptedException {
-        WaitingThread thread = new WaitingThread(_semaphore, 0);
+        WaitingThread thread = new WaitingThread(_semaphore, 9999999);
         thread.start();
         Thread.sleep(1);
         assertFalse("The thread should be waiting.", thread.hasFinished());
